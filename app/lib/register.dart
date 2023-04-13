358c57d613b94d6bb55699b3e4e85a7114c6fd22
tiagofcviana <tfilipecviana@gmail.com>
2023-04-13 18:11:08 +0100

    Template code for the registration

---
+++ app/lib/register.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _email;
+  late String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your email address';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _email = value!;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a password';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _password = value!;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // TODO: call the registration API with email and password
+                  }
+                },
+                child: const Text('Register'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
