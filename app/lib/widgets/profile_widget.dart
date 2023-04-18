bd3825f288ef27cd68998886b66845c56e0c606f
tiagofcviana <tfilipecviana@gmail.com>
2023-04-18 15:44:48 +0100

    Profile page updated

--- app/lib/widgets/profile_widget.dart
+++ app/lib/widgets/profile_widget.dart
@@ -10,24 +10,95 @@ class ProfileWidget extends StatefulWidget {
 }
 
 class _ProfileWidgetState extends State<ProfileWidget> {
+  User? user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
-      body: Stack(
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('You are logged in!'),
-              Text(
-                user.email!,
+          const SizedBox(height: 50),
+          CircleAvatar(
+            radius: 60,
+            backgroundImage: user?.photoURL != null ? NetworkImage(user!.photoURL!) : null,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            user?.displayName ?? '',
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            user?.email ?? '',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[600],
+            ),
+          ),
+          const SizedBox(height: 40),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
               ),
-              ElevatedButton(
-                onPressed: () => FirebaseAuth.instance.signOut(),
-                child: const Text('Sign out'),
-              )
-            ],
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    const Text(
+                      'Profile Information',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    _buildProfileInfoItem('Full Name', user?.displayName ?? ''),
+                    _buildProfileInfoItem('Email Address', user?.email ?? ''),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => FirebaseAuth.instance.signOut(),
+                      child: const Text('Sign Out'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileInfoItem(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+            ),
+          ),
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
           ),
         ],
       ),
