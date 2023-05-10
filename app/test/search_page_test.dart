cad19b83b37db1a866a230fa27e45267bb6a1a35
Pedro <89313201+pramadureira@users.noreply.github.com>
2023-05-10 17:35:36 +0100

    Fixed a unit test dependency

--- app/test/search_page_test.dart
+++ app/test/search_page_test.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sportspotter/main.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 
 void main() {
   group('Search Page', () {
   testWidgets('Display search bar when I click on the search page button', (
       WidgetTester tester) async {
+    databaseFactory = databaseFactoryFfi;
     await tester.pumpWidget(MyApp());
     final searchButton = find.text('Search');
     expect(searchButton, findsOneWidget);
